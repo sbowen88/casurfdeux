38c6808989fe500d1bd2694c114bddecfeefd60c
sbowen88 <sbowey@gmail.com>
2019-03-04 12:13:13 -0800

    routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-    <h2>Wazzzzah</h2>
+        <h2>Wazzzzah</h2>
       </div>
     );
   }
