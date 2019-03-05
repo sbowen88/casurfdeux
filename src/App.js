d0f6f73a05d72e53b5186c79ef045d984bedf43b
sbowen88 <sbowey@gmail.com>
2019-03-05 12:39:06 -0800

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-
+import Navbar from "./Components/Navbar/Navbar";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div classNameName="App">
+        <Navbar />
         <h2>Wazzzzah</h2>
       </div>
     );
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 
 class Homepage extends Component {
